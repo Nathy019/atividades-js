2d9df057972e0f34aacfa7d4426b9609223bcc4e
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2022-12-08 19:12:09 -0300

    ex-008
     Escreva um programa que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo. Para saber o valor do pi em JavaScript, use: Math.PI

---
+++ ex-008/apps.js
@@ -0,0 +1,6 @@
+var raio= prompt ("Raio do circulo")
+var raio= parseFloat (raio)
+var comprimento = 2* Math.PI * raio 
+var area = Math.PI  * raio * raio
+ document.write("comprimento da circunferencia = ", comprimento ,"<br>")
+ document.write("area da circunferencia =", area)
